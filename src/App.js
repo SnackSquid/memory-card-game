54fd9e2e36aed012ba1cfa06fa5eedfea4bb2149
Spencer Williams <Spilliams@gmail.com>
2022-08-30 16:40:34 -0500

    Set city list and begin board generation function

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  // use state to maintain a list of cities and if they've
+  // been clicked or not
+  const [cityList, updateCityList] = useState({
+    "New York City": false,
+    "Los Angeles": false,
+    Chicago: false,
+    Houston: false,
+    Phoenix: false,
+    Philadelphia: false,
+    "San Antonio": false,
+    "San Diego": false,
+    Dallas: false,
+    "San Jose": false,
+    Austin: false,
+    Jacksonville: false,
+    "Fort Worth": false,
+  });
+
+  useEffect(() => {
+    // to do
+  }, [cityList]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="cardContainer">
+      <CardBuilder citylist={cityList} />
     </div>
   );
+}
+
+function CardBuilder(props) {
+  const cityList = Object.keys(props.citylist)
+  const gameBoard = [];
+  for (let i = 0; i < cityList.length; i++) {
+    const cityName = cityList[i];
+    gameBoard.concat(city);
+  }
+
+  return gameBoard;
 }
 
 export default App;
