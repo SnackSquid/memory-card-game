35ab2e60d709b3485dab887bcbd811f9cf894ced
Spencer Williams <Spilliams@gmail.com>
2022-08-31 12:12:33 -0500

    Add game loop and score count

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
-import newyorkcity from "./images/triston-dunn-rfj_wOYQkus-unsplash.jpg";
 
 function App() {
   // use state to maintain a list of cities and if they've
@@ -20,21 +19,48 @@ function App() {
     Jacksonville: false,
     "Fort Worth": false,
   });
+  const [score, updateScore] = useState(0);
 
-  useEffect(function clickCard() {
-    // to do 
-    < CardBuilder citylist = { cityList } />
+  const handleClick = (props) => {
+    // get the current city being clicked on
+    const cityName = props.className;
+    // make sure the input is valid
+    if (cityName === "" || cityName === undefined) {
+      return
+    }
+    // create true entry for city for immutability
+    const clickedCity = {[cityName]: true};
+    if (!cityList[cityName]) {
+      updateCityList(city => ({
+        ...city,
+        ...clickedCity
+      }));
+      updateScore(score + 1);
+    } else {
+      const falseList = Object.assign({}, cityList);
+      Object.keys(falseList).forEach(key => {
+        falseList[key] = false;
+      });
+      console.log(falseList)
+      updateCityList(falseList)
+      updateScore(0);
+    }
+    
+  }
+
+  useEffect(() => {
+    <CardBuilder citylist = { cityList } />
   }, [cityList]);
 
   return (
     <div>
       <div className="header">
         <h1>Card Game Thing</h1>
-        <p></p>
+        <p>{score}</p>
       </div>
       <div className="cardContainer">
         <CardBuilder
-          onClick={useEffect(clickCard)}
+          onClick={handleClick}
           citylist={cityList} />
       </div>
     </div>
@@ -42,13 +68,22 @@ function App() {
 }
 
 function CardBuilder(props) {
-
+  // click handler passes up the current target
+  const handleClick = (event) => {
+    event.stopPropagation();
+    props.onClick(event.target);
+  }
+  // get just the city names
   const cityList = Object.keys(props.citylist)
+  // use map to take the city name and create the city card
   const gameBoard = cityList.map((city => 
-  <div key={city} onClick={props.onClick}>
+  <div key={city} 
+  onClick={handleClick}>
     <img className={city} alt={city} />
     <h3>{city}</h3>
     </div>));
+
+  
   // credit for sorting algorithm to
   // Nitin Patel && Donald E. Knuth
   // https://medium.com/@nitinpatel_20236/how-to-shuffle-correctly-shuffle-an-array-in-javascript-15ea3f84bfb
